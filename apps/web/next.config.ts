0fc51cd81889ce546c14586f03ca07c85ba5b5c3
unknown <namapur051@gmail.com>
2026-06-15 18:06:33 +0700

    Fix Prisma Engine on Vercel

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  outputFileTracingIncludes: {
+    "/*": ["../../apps/api/prisma/generated/client/**/*"],
+  },
 };
 
 export default nextConfig;
